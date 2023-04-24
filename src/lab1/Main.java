06b2680efac0be0f8c98c5bae1b0e5caf6c8fe4d
grisha <test@gmail.com>
2023-04-24 20:52:20 +0300

    методи для пошуку книг

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -32,4 +32,29 @@ public class Main {
         main.bookArrayList.add(book2);
         main.bookArrayList.add(book3);
     }
+
+    public String searchAuthor(String author){
+        String result = "";
+        for (int i = 0; i < bookArrayList.size(); i++ ){
+            if(author.equals(bookArrayList.get(i).getAuthor()))
+                result += "\n\n"+ bookArrayList.get(i).toString();
+        }
+        return result;
+    }
+    public String searchPublisher(String publication){
+        String result = "";
+        for (int i = 0; i < bookArrayList.size(); i++ ){
+            if(publication.equals(bookArrayList.get(i).getPublication()))
+                result += "\n\n"+ bookArrayList.get(i).toString();
+        }
+        return result;
+    }
+    public String searchYearPublication(int year){
+        String result = "";
+        for (int i = 0; i < bookArrayList.size(); i++ ){
+            if(bookArrayList.get(i).getYearPublication() >= year)
+                result += "\n\n"+ bookArrayList.get(i).toString();
+        }
+        return result;
+    }
 }
