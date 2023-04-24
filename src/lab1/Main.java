0516ad6ca2b6cfcdfab0d93c567ff345d21a8f7b
grisha <test@gmail.com>
2023-04-24 20:50:19 +0300

    масив книг

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,6 +1,35 @@
 package lab1;
-
+import java.util.ArrayList;
 
 public class Main {
+    private ArrayList<Book> bookArrayList = new ArrayList<Book>();
+    public static void main(String[] args) {
+        Main main = new Main();
+        Book book1 = new Book(1,
+                "Книга 1",
+                "Перший автор",
+                "Видавництво Букварик",
+                2001,
+                352,
+                300);
 
+        Book book2 = new Book(2,
+                "Книга 2",
+                "Другий автор",
+                "Видавництво Щоденник",
+                2011,
+                152,
+                200);
+        Book book3 = new Book(3,
+                "Книга 3",
+                "Третій автор",
+                "Видавництво Метелик",
+                2017,
+                408,
+                400);
+
+        main.bookArrayList.add(book1);
+        main.bookArrayList.add(book2);
+        main.bookArrayList.add(book3);
+    }
 }
